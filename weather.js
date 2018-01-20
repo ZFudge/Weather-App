e3e6dce625855f1dc1fe133e957ac0f06eb25159
zfudge <zacheryfudge@gmail.com>
2018-01-20 08:57:54 -0700

    fahrenheit/celsius conversion

--- weather.js
+++ weather.js
@@ -1,24 +1,38 @@
-let pos, response;
-const para = document.getElementById('response');
-const icon = document.getElementById('icon');
-const temp = document.getElementById('temp');
+const app = {
+  main: function() {
+      writeResponse();
+      writePic();
+      writeTemp();
+  }
+};
 
-navigator.geolocation.getCurrentPosition((p)=>pos=p);
+const weather = {
+  para: document.getElementById('response'),
+  icon: document.getElementById('icon'),
+  temp: document.getElementById('temp'),
+  fahrenheit: true
+};
+
+navigator.geolocation.getCurrentPosition((p)=>app.position=p);
 
 function update() {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
-      response = JSON.parse(xhr.response);
-      writeResponse();
-      writePic();
-      writeTemp();
+      app.response = JSON.parse(xhr.response);
+      app.main();
     }
   }
-  xhr.open("GET", `https://fcc-weather-api.glitch.me/api/current?lat=${Math.round(pos.coords.latitude)}&lon=${Math.round(pos.coords.longitude)}`);
+  xhr.open("GET", `https://fcc-weather-api.glitch.me/api/current?lat=${Math.round(app.position.coords.latitude)}&lon=${Math.round(app.position.coords.longitude)}`);
   xhr.send();
 }
 
-const writeResponse = () => para.innerHTML = response.weather[0].description;
-const writePic = () => icon.src = response.weather[0].icon;
-const writeTemp = () => temp.innerHTML = response.main.temp;
+const writeResponse = () => weather.para.innerHTML = app.response.weather[0].description;
+const writePic = () => weather.icon.src = app.response.weather[0].icon;
+const writeTemp = () => weather.temp.innerHTML = `Temp: ${detTemp(app.response.main.temp)}  High: ${detTemp(app.response.main.temp_max)}  Low: ${detTemp(app.response.main.temp_min)}`;
+const detTemp = (c) => (weather.fahrenheit) ? (c * (9/5) + 32).toFixed(1) : c;
+
+function convert() {
+  weather.fahrenheit = ! weather.fahrenheit;
+  writeTemp();
+}
